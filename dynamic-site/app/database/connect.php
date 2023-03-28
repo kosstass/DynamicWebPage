cf736a65b058875a6b0e54f9f27c6ad9597e157f
kosstass <127876356+kosstass@users.noreply.github.com>
2023-03-28 16:47:17 +0300

    dynamic
    dynamic web page

---
+++ dynamic-site/app/database/connect.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+$driver = 'mysql';
+$host = 'localhost';
+$db_name = 'mysite';
+$db_user = 'root';
+$db_pass = 'Kvardakov101';
+$charset = 'utf8';
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+
+try{
+    $pdo = new PDO(
+        "$driver:host=$host;dbname=$db_name;charset=$charset", $db_user, $db_pass, $options
+    );
+}catch (PDOException $i){
+    die("Ошибка подключения к базе данных");
+}
